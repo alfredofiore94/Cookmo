65058c917ffd2094a1dd571a2e59b2975f1815c0
Alfredo Fiore <alfredofiore@live.com>
2022-08-12 17:41:47 +0200

    Initial Commit

---
+++ CookMo/CookMoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookMoApp.swift
+//  CookMo
+//
+//  Created by Alfredo Fiore on 12/08/22.
+//
+
+import SwiftUI
+
+@main
+struct CookMoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
