94ffd04c2cd90a399199ef3786dca91583a97813
Alfredo Fiore <alfredofiore@live.com>
2022-08-16 18:36:05 +0200

    Aggiunta evento click to next tab, tab iniziale

--- CookMo/tabBar_custom/CustomMainTabBar.swift
+++ CookMo/tabBar_custom/CustomMainTabBar.swift
@@ -9,17 +9,28 @@ import SwiftUI
 
 struct CustomMainTabBar: View {
     
+    
+    
     let listaTabs: [CustomTabBarItem]
+    
+    //imposta il tab iniziale anche
+    @Binding var tabSelezionato : CustomTabBarItem
+    
     let coloreSfondo: Color
+    let coloreSelezione: Color
     
     var body: some View {
         HStack{
             
             ForEach(listaTabs, id: \.self){
                 tab in tabView(tabItem: tab)
+                    .onTapGesture {
+                        switchToTaItem(tab: tab)
+                    }
             }
         }
         .background(coloreSfondo)
+        
     }
 }
 
@@ -31,11 +42,18 @@ extension CustomMainTabBar {
                 .renderingMode(.template)
             Text(tabItem.titolo)
         }
-        .foregroundColor(tabItem.colore)
+        .foregroundColor(tabSelezionato == tabItem ? coloreSelezione : tabItem.colore)
         .padding(.vertical, 10)
         .frame(maxWidth: .infinity)
         .background(tabItem.coloreSfondo)
         
+    }
+    
+    private func switchToTaItem(tab: CustomTabBarItem){
+        
+        withAnimation(.easeInOut) {
+            tabSelezionato = tab
+        }
     }
    
     
@@ -55,12 +73,16 @@ struct CustomMainTabBar_Previews: PreviewProvider {
         CustomTabBarItem(nomeIcona: "bilancia_img", titolo: "Bilancia", colore: Color.white, coloreSfondo: Color.white.opacity(0)),
         CustomTabBarItem(nomeIcona: "categorie_img", titolo: "Categorie", colore: Color.white, coloreSfondo: Color.white.opacity(0)),
         CustomTabBarItem(nomeIcona: "home_img", titolo: "Home", colore: Color.white,  coloreSfondo: Color.white.opacity(0)),
-        CustomTabBarItem(nomeIcona: "cerca_img", titolo: "Cerca", colore: Color.white,  coloreSfondo: Color.white.opacity(0))
+        CustomTabBarItem(nomeIcona: "cerca_img", titolo: "Cerca", colore: Color.white,  coloreSfondo: Color.clear)
     ]
     
     
     static var previews: some View {
-        CustomMainTabBar(listaTabs: tabs, coloreSfondo: Color(red: 227 / 255, green: 124 / 255, blue: 81 / 255))
+        VStack{
+            Spacer()
+            CustomMainTabBar(listaTabs: tabs,tabSelezionato: .constant(tabs[2]) , coloreSfondo: Color(red: 227 / 255, green: 124 / 255, blue: 81 / 255), coloreSelezione: Color.blue)
+        }
+        
     }
 }
 
