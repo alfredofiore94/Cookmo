d1890ef4631b55f9a164b9ce6fa63c18c16e6b57
Alfredo Fiore <alfredofiore@live.com>
2022-08-20 22:43:31 +0200

    Aggiunta costruttuore, icone dettagli ricetta, custom oggetto dettaglio

--- CookMo/RicettaCard.swift
+++ CookMo/RicettaCard.swift
@@ -8,6 +8,24 @@
 import SwiftUI
 
 struct RicettaCard: View {
+    
+    var nomePiatto: String
+    var tipoPiatto: String
+    var difficolta: String
+    var tempoPrep: String
+    var costo : String
+    
+    
+    init(nomePiatto: String, tipoPiatto: String, difficolta: String, tempoPrep: String, costo : String){
+        
+        self.nomePiatto = nomePiatto
+        self.tipoPiatto = tipoPiatto
+        self.difficolta = difficolta
+        self.tempoPrep = tempoPrep
+        self.costo = costo
+        
+    }
+    
     var body: some View {
         ZStack{
             RoundedRectangle(cornerRadius: 15)
@@ -15,9 +33,9 @@ struct RicettaCard: View {
                 .stroke(ContentView.coloreMoveo, lineWidth: 5)
             HStack(alignment: .top){
                 VStack (alignment: .leading){
-                    Text("DOLCI")
+                    Text(self.tipoPiatto)
                         .font(.system(size: 18))
-                    Text("PANDORO")
+                    Text(self.nomePiatto)
                         .padding(.leading)
                         .padding(.bottom)
                         .padding(.top)
@@ -27,9 +45,11 @@ struct RicettaCard: View {
                 .padding()
                 Spacer()
                 VStack(alignment: .leading){
-                    Text("Difficile")
-                    Text("48 ore")
-                    Text("10,50 €")
+
+                    InfoRicetta(self.difficolta, "chef_img")
+                    InfoRicetta(self.tempoPrep, "timer_img")
+                    InfoRicetta(self.costo, "coin_img")
+
                     
                 }
                 .font(.system(size: 12))
@@ -47,8 +67,29 @@ struct RicettaCard: View {
     }
 }
 
+struct InfoRicetta: View {
+    
+    var nome: String
+    var immagine: String
+
+    init(_ nome: String, _ immagine: String) {
+        self.nome = nome
+        self.immagine = immagine
+
+    }
+
+    var body: some View {
+        HStack{
+            Image(immagine)
+                .renderingMode(.template)
+            Text(nome)
+        }
+        
+    }
+}
+
 struct RicettaCard_Previews: PreviewProvider {
     static var previews: some View {
-        RicettaCard()
+        RicettaCard(nomePiatto: "Pandoro", tipoPiatto: "Dolce", difficolta: "Media", tempoPrep: "48 ore", costo: "16,50€")
     }
 }
